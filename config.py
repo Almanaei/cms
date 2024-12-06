a022834625fa81c5d1b48b405a53907b163496b3
Almanaei <almannaei90@gmail.com>
2024-12-06 22:39:41 +0300

    Update requirements.txt with psycopg2-binary and simplify database config

--- config.py
+++ config.py
@@ -8,7 +8,9 @@ load_dotenv()
 class Config:
     # Basic Config
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    
+    # Database Config
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
