7797abf803b062cc3173e5199c18d4693bfc8c9a
Almanaei <almannaei90@gmail.com>
2024-12-06 15:15:05 +0300

    Remove Flask-Migrate dependency from app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 from config import Config
 from datetime import datetime
 
 db = SQLAlchemy()
 login_manager = LoginManager()
-migrate = Migrate()
 csrf = CSRFProtect()
 
 def create_app(config_class=Config):
@@ -16,7 +14,6 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     db.init_app(app)
-    migrate.init_app(app, db)
     login_manager.init_app(app)
     csrf.init_app(app)
 
