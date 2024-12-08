ef4eda7013e1d5649539ce21b05319222dcda6aa
Almanaei <almannaei90@gmail.com>
2024-12-08 21:53:06 +0300

    Fix imports in check_migration.py to use create_app()

--- check_migration.py
+++ check_migration.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, current
-from app import app, db
+from app import create_app, db
 
+app = create_app()
 migrate = Migrate(app, db)
 
 if __name__ == '__main__':
